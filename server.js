d896f8716eda14203acd0b3eec7d3d1aa83ed059
Muhammad Furqan <muhammadfurqan3624515@gmail.com>
2025-12-02 10:52:53 +0500

    updating cors

--- server.js
+++ server.js
@@ -8,14 +8,25 @@ import userRoutes from "./routes/userRoutes.js";
 dotenv.config();
 const app = express();
 
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173", // local development
+      "https://lost-and-found-front-end-pearl.vercel.app", // your Vercel frontend
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 // routes
 app.use("/api/lost", lostRoutes);
 app.use("/api/found", foundRoutes);
 app.use("/api/users", userRoutes);
-
 
 console.log("Backend restarted at:", Date.now());
 
